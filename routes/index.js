d3776cb3218f07dbbc128de9048fda38ba23aa1d
mjoseba@gmail.com <mjoseba@gmail.com>
2015-07-23 09:52:21 +0200

    fixes

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-//Controlador:
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+/* Página de entrtada (home page) */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.param('quizId', quizController.load);
+// Autoload de comandos con ids
+router.param('quizId', quizController.load);  // autoload :quizId
 
-router.get('/quizes)', quizController.index);
+// Definición de rutas /quizes
+router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-
-// GET author
 router.get('/author', function(req, res) {
   res.render('author');
 });
