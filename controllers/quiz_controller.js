468635e4731747f6da185462158736feb92ff7a3
mjoseba@gmail.com <mjoseba@gmail.com>
2015-07-23 08:39:29 +0200

    Base de datos postgree en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,47 @@
+var models = require('../models/models.js')
+
+//Auto load
+
+exports.load = function(req, res){
+  models.Quiz.find(quizId).success(function(quiz){
+    if (quiz){
+      req.quiz = quiz;
+      next();
+    }else{
+      next(new Error('No existe quizId='+quizId));
+    }
+
+  }).catch(function(error){next(error);});
+};
+
+
+//GET /quizes
+exports.index = function(req, res){
+  models.Quiz.findAll().success(function(quiz){
+    res.render('quizes/index.ejs', {quizes: quizes});
+  });
+};
+
 //GET /quizes/question
-exports.question = function(req, res){
-  res.render('quizes/question', {pregunta: "Capital de Italia"});
+exports.show = function(req, res){
+  models.Quiz.findAll().success(function(quiz){
+    res.render('quizes/show', {quiz: req.quiz});
+  });
 };
 
 //GET /quizes/answer
 exports.answer = function(req, res){
-  if( req.query.respuesta === "Roma"){
-    res.render('quizes/answer', {respuesta: "Correcto"});
-  }else{
-    res.render('quizes/answer', {respuesta: "Incorrecto"});
-  }
+
+  models.Quiz.findAll().success(function(quiz){
+    var resultado = 'Incorrecto';
+    if( req.query.respuesta === req.quiz.respuesta){
+      resultado = 'Correcto';
+    }
+
+    res.render('quizes/answer', {quiz:req.quiz, respuesta: resultado})
+
+  });
+
+
+
 };
--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+router.param('quizId', quizController.load);
+
+router.get('/quizes)', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 // GET author
 router.get('/author', function(req, res) {
